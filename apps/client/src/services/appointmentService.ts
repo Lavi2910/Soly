62cc8aeaca2aafa34dc8e1557d768a14d7ae72ca
Lavi2910 <almog.lavi29@gmail.com>
2026-04-06 16:24:55 +0300

    feat: add API service layer with Axios

---
+++ apps/client/src/services/appointmentService.ts
@@ -0,0 +1,27 @@
+import api from './api';
+
+export async function bookAppointment(
+  time: Date,
+  providerId: string,
+  serviceId: string,
+) {
+  const response = await api.post('/appointments', {
+    time,
+    providerId,
+    serviceId,
+  });
+  return response.data;
+}
+
+export async function getAppointments() {
+  const response = await api.get('/appointments');
+  return response.data;
+}
+
+export async function updateAppointment(
+  id: string,
+  data: { status?: string; time?: Date },
+) {
+  const response = await api.put('/appointments/' + id, data);
+  return response.data;
+}
